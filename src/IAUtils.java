6366607da6788990e2f90379edbe0c4b00838aab
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2018-10-01 02:42:06 +0200

    Clamp + Random number generation

--- src/IAUtils.java
+++ src/IAUtils.java
@@ -1,4 +1,28 @@
 package src;
 
 public class IAUtils {
+
+    // if val < min -> min
+    // if val > max -> max
+    // return val
+    public static <T extends Comparable<T>> T clamp(T val, T min, T max) {
+        if (val.compareTo(min) < 0) return min;
+        else if (val.compareTo(max) > 0) return max;
+        else return val;
+    }
+
+
+    // Random
+    public static int random() {
+        return (int)(Math.random());
+    }
+
+    public static int random(int min, int max) {
+        return (int)(Math.random() * (max - min) + min);
+    }
+
+    public static double random(double min, double max) {
+        return Math.random() * (max - min) + min;
+    }
+
 }
