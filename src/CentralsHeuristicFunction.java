76c3d3c903988be9efce437f4c1622bd8af9381a
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2018-10-14 13:57:27 +0200

    Update entropía

--- src/CentralsHeuristicFunction.java
+++ src/CentralsHeuristicFunction.java
@@ -21,6 +21,36 @@ public class CentralsHeuristicFunction implements HeuristicFunction {
 	}
 
 	public double HeuristicFunction1(CentralsRepresentation state) {
+
+		// Coste centrales
+
+		// Beneficio clientes
+
+		// Entropia solucion
+		if (state.hCliente_old != -1) { // no es la solución inicial
+
+			if (state.hCliente_new == -1) { // assign
+				double dist_old = getDistacia(state.hCentral_old, state.hCliente_old);
+				double dist_new = getDistacia(state.hCentral_new, state.hCliente_old);
+				double perdida_old = VEnergia.getPerdida(dist_old);
+				double perdida_new = VEnergia.getPerdida(dist_new);
+				state.entropia = state.entropia - perdida_old + perdida_new;
+			}
+			else {  // swap
+				double dist_old_old = getDistacia(state.hCentral_old, state.hCliente_old);
+				double dist_new_old = getDistacia(state.hCentral_new, state.hCliente_old);
+				double dist_old_new = getDistacia(state.hCentral_old, state.hCliente_new);
+				double dist_new_new = getDistacia(state.hCentral_new, state.hCliente_new);
+				double perdida_old_old = VEnergia.getPerdida(dist_old_old);
+				double perdida_new_old = VEnergia.getPerdida(dist_new_old);
+				double perdida_old_new = VEnergia.getPerdida(dist_old_new);
+				double perdida_new_new = VEnergia.getPerdida(dist_new_new);
+				state.entropia = state.entropia - perdida_old_old + perdida_new_old;
+				state.entropia = state.entropia - perdida_new_new + perdida_old_new;
+			}
+
+		}
+
 		double heuristico = state.beneficio * (1.0D - (state.entropia / (double)CentralsRepresentation.clients.size()));
 
 		return -heuristico;
