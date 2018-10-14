7843d34c9f051d2a65a5a36f1ef9642e4ef5d3ca
Joaquim Gómez <9joaquimgomez@gmail.com>
2018-10-14 15:48:56 +0200

    Heuristic more clever

--- src/CentralsHeuristicFunction.java
+++ src/CentralsHeuristicFunction.java
@@ -1,5 +1,7 @@
 package src;
 
+import IA.Energia.Central;
+import IA.Energia.Cliente;
 import aima.search.framework.HeuristicFunction;
 import IA.Energia.VEnergia;
 
@@ -13,14 +15,71 @@ public class CentralsHeuristicFunction implements HeuristicFunction {
 	public double getHeuristicValue(Object s) {
 		CentralsRepresentation state = (CentralsRepresentation) s;
 
-		return HeuristicFunction1(state);
+		try {
+			return HeuristicFunction1(state);
+		}
+        catch (Exception e) {
+			System.out.println("Excepción: " + e);
+			return 0.0;
+		}
 	}
 
-	public double HeuristicFunction1(CentralsRepresentation state) {
+	public double HeuristicFunction1(CentralsRepresentation state) throws Exception {
 
 		// Coste centrales
+		if (state.hCliente_old != -1) { // no es la solución inicial
+
+			if (state.hCliente_new == -1) { // assign
+
+				if (state.representationCentrales[state.hCentral_old] == 0){ // se apaga una central old
+					Central cent = CentralsRepresentation.centrals.get(state.hCentral_old);
+                    state.beneficio += VEnergia.getCosteMarcha(cent.getTipo());
+					state.beneficio += VEnergia.getCosteProduccionMW(cent.getTipo());
+					state.beneficio -= VEnergia.getCosteParada(cent.getTipo());
+				}
+				if (state.representationCentrales[state.hCentral_new] == 0) {  // se apaga una central new
+                    Central cent = CentralsRepresentation.centrals.get(state.hCentral_new);
+					state.beneficio += VEnergia.getCosteMarcha(cent.getTipo());
+					state.beneficio += VEnergia.getCosteProduccionMW(cent.getTipo());
+					state.beneficio -= VEnergia.getCosteParada(cent.getTipo());
+				}
+				if (state.representationCentrales[state.hCentral_new]
+						== CentralsRepresentation.clients.get(state.hCliente_old).getConsumo()) { // se enciende central
+					Central cent = CentralsRepresentation.centrals.get(state.hCentral_new);
+					state.beneficio -= VEnergia.getCosteMarcha(cent.getTipo());
+					state.beneficio -= VEnergia.getCosteProduccionMW(cent.getTipo());
+					state.beneficio += VEnergia.getCosteParada(cent.getTipo());
+				}
+
+			}
+			else {	// swap
+                // no cambia nada
+			}
+
+		}
 
 		// Beneficio clientes
+		if (state.hCliente_old != -1) { // no es la solución inicial
+
+			if (state.hCliente_new == -1){ // assign
+				Cliente c = state.clients.get(state.hCliente_old);
+
+				if (state.hCentral_old == 1) { // fuera a dentro
+					if (c.getContrato() == Cliente.NOGARANTIZADO) {
+						state.beneficio += VEnergia.getTarifaClienteNoGarantizada(c.getTipo()) * c.getConsumo();
+					}
+					else {
+						state.beneficio += VEnergia.getTarifaClienteGarantizada(c.getTipo()) * c.getConsumo();
+					}
+				}
+				else {  // de una central a otra
+					// solo cambia el beneficio si la central se apaga (se comprueba en el coste de centrales
+				}
+			}
+			else {  // swap
+				// en swap de clientes entre centrales no afecta al beneficio
+			}
+		}
 
 		// Entropia solucion
 		if (state.hCliente_old != -1) { // no es la solución inicial
